0763c12affb936d3e926524ce180e411ba184b77
RiskyAgungPrasetyo <riskyagungprasetyo@student.uir.ac.id>
2023-05-09 15:41:23 +0700

    Add your commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>Selamat datang di Web Aplikasi Sederhana! Kami adalah sebuah tim yang terdiri dari pengembang web yang bersemangat untuk menciptakan solusi teknologi yang efektif dan efisien bagi pengguna.
+Misi kami adalah untuk menyediakan aplikasi web sederhana namun bermanfaat untuk membantu memudahkan pekerjaan sehari-hari Anda. Kami percaya bahwa teknologi harus mudah diakses dan digunakan oleh semua orang, tanpa memerlukan pengalaman teknis yang tinggi.
+Tim kami memiliki pengalaman yang luas dalam pengembangan web dan teknologi terkait. Kami sangat mengutamakan kualitas dan keamanan produk kami, sehingga kami selalu memastikan bahwa setiap aplikasi yang kami buat telah melalui pengujian dan evaluasi yang ketat sebelum diluncurkan.
+Kami juga menghargai umpan balik dan masukan dari pengguna kami, sehingga kami selalu berusaha untuk meningkatkan layanan dan mengintegrasikan fitur-fitur baru yang dapat membantu memenuhi kebutuhan pengguna.</p>
+  </div>
+</template>
+<style>
+  .about{
+    color: white;
+    max-width: 800px;
+    margin: 0 auto;
+    padding: 20px;
+    border-radius: 10px;
+    box-shadow: 0 3px 5px rgba(0,0,0,.1);
+  }
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <h1>WELCOME TO OUR WEBSITE</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+<style>
+.home{
+  color: white;
+}
+</style>
